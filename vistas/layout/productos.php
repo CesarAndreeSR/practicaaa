d0e33feb737a069052a2e10a3db006da1f2a25ae
Cesar Andree <andrechi999@gmail.com>
2024-05-27 15:02:45 -0500

    new productos

--- vistas/layout/productos.php
+++ vistas/layout/productos.php
@@ -15,9 +15,10 @@
           </div>
         </div>
         <div class="card-body">
-          <button class="btn btn-secondary" style="background-color: #343a40;" data-toggle="modal" data-target="#modalAgregarCategoria">
-          <i class="nav-icon fas fa-box-open"> Agregar Productos</i>  
-          
+          <button class="btn btn-secondary" style="background-color: #343a40;" data-toggle="modal"
+            data-target="#modalAgregarCategoria">
+            <i class="nav-icon fas fa-box-open"> Agregar Productos</i>
+
           </button>
         </div>
         <!-- /.card-body -->
@@ -32,32 +33,80 @@
 </div>
 <!--modal categoria-->
 <!-- Modal -->
+<style>
+  .container {
+    display: flex;
+  }
+
+  .elemento {
+    width: 44.33%;
+    float: left;
+    margin-left: 3.33%;
+  
+  }
+  select{
+    width: 185px;
+  }
+</style>
 <div class="modal fade" id="modalAgregarCategoria" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
   aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <div class="modal-content">
+    <div class="modal-content" style="display: grid; width:700px">
+
       <form method="post" role="form">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel"><i class="nav-icon fab fa-wpforms"> Formulario Agregar Categoría</i></h5>
+          <h5 class="modal-title" id="exampleModalLabel"><i class="nav-icon fab fa-wpforms"> Formulario
+              Agregar Categoría</i></h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div class="modal-body">
+        <div class="modal-body" >
           <div class="box-body">
             <div class="form-group">
               <div class="input-group">
-                
-                <input type="text" class="form-control input-lg" placeholder="Ingresar Categoria" required
-                  name="nuevaCategoria">
+                <div class="container">
+                  <div class="elemento">Elemento 1
+                    <div>
+                      <i>Seleccione categoria</i><br> 
+                      <select name="" id="" >
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                        <option value="">4 </option>
+                      </select><br>
+                      <i class="fa fa-user" aria-hidden="true"> Stock Productos</i>
+                      <input type="number" name="" id=""><br>
+                      <i>precio al x mayor</i>
+                      <input type="number" name="" id="">
+                    </div>
+
+
+                  </div>
+                  <div class="elemento">Elemento 2
+                    <div>
+                      <i>codigo del producto</i>
+                      <input type="number">
+                    </div>
+                  </div>
+                  <div class="elemento">Elemento 3
+                    <div>
+                      <i>Descripccion del producto</i>
+                      <input type="text">
+                    </div>
+                  </div>
+                  
+                </div>
               </div>
             </div>
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #343a40;"><i class="nav-icon fas fa-window-close"> Cerrar</i></button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #343a40;"><i
+              class="nav-icon fas fa-window-close"> Cerrar</i></button>
           <button type="button" class="btn btn-primary"><i class="nav-icon fa fa-save"></i> Guardar</button>
         </div>
+
       </form>
 
     </div>
--- vistas/layout/usuarios.php
+++ vistas/layout/usuarios.php
@@ -1,42 +1,43 @@
 <div class="content-wrapper">
-    <div class="container-header">
-        <!-- /.col-md-6 -->
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="fa fa-address-book" aria-hidden="true"> Modulo de Registrar Usuarios</h3>
+  <div class="container-header">
+    <!-- /.col-md-6 -->
+    <div class="col-lg-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="fa fa-address-book" aria-hidden="true"> Modulo de Registrar Usuarios</h3>
 
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                            <i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <button type="button" class="btn btn-secondary" data-toggle="modal"
-                        style="background-color: #343a40;" data-target="#ModalAgregarUsuario" >
-                        <i class="fa fa-address-book" aria-hidden="true"> Agregar Usuarios</i>
-                        
-                    </button>
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          <button type="button" class="btn btn-secondary" data-toggle="modal" style="background-color: #343a40;"
+            data-target="#ModalAgregarUsuario">
+            <i class="fa fa-address-book" aria-hidden="true"> Agregar Usuarios</i>
 
-                </div>
-                <!-- /.card-body -->
-
-            </div>
+          </button>
 
         </div>
-        <!-- /.col-md-6 -->
+        <!-- /.card-body -->
 
-        <!-- /.row -->
-    </div><!-- /.container-fluid -->
+      </div>
+
+    </div>
+    <!-- /.col-md-6 -->
+
+    <!-- /.row -->
+  </div><!-- /.container-fluid -->
 </div>
 
 <!-- Modal Agregar Usuarios -->
 <!-- Modal Agregar Usuarios -->
-<div class="modal fade" id="ModalAgregarUsuario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalAgregarUsuario" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <form method="post" role="form">
@@ -51,25 +52,25 @@
           <div class="row">
             <div class="col-md-6">
               <div class="form-group">
-              <i class="fa fa-id-card" aria-hidden="true"> Nombre</i>
+                <i class="fa fa-id-card" aria-hidden="true"> Nombre</i>
                 <input type="text" class="form-control" id="nombre" placeholder="Ingresar nombre" required>
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
-              <i class="fa fa-user-circle" aria-hidden="true"> Usuario</i>
+                <i class="fa fa-user-circle" aria-hidden="true"> Usuario</i>
                 <input type="text" class="form-control" id="usuario" placeholder="Ingresar usuario" required>
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
-              <i class="fa fa-unlock-alt" aria-hidden="true"> contraseña</i>
+                <i class="fa fa-unlock-alt" aria-hidden="true"> contraseña</i>
                 <input type="password" class="form-control" id="password" placeholder="Ingresar contraseña" required>
               </div>
             </div>
             <div class="col-md-6">
               <div class="form-group">
-              <i class="fa fa-user" aria-hidden="true"> Perfil</i>
+                <i class="fa fa-user" aria-hidden="true"> Perfil</i>
                 <input type="text" class="form-control" id="perfil" placeholder="Ingresar perfil" required>
               </div>
             </div>
@@ -78,14 +79,16 @@
                 <div class="panel">
                   <input type="file" id="nuevaFoto" name="nuevaFoto" class="nuevaFoto" onchange="previewImage(event)">
                   <p class="help-block">Peso máximo de la foto 200 MB</p>
-                  <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px" id="imgPreview">
+                  <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px"
+                    id="imgPreview">
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #343a40;">Cerrar</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal"
+            style="background-color: #343a40;">Cerrar</button>
           <button type="button" class="btn btn-primary">Guardar</button>
         </div>
 
@@ -99,9 +102,9 @@
 </div>
 
 <script>
-  function previewImage(event){
+  function previewImage(event) {
     var reader = new FileReader();
-    reader.onload = function(){
+    reader.onload = function () {
       var output = document.getElementById('imgPreview');
       output.src = reader.result;
     }
